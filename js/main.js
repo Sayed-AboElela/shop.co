aa321e315943dd6071dd03b4be0db2bddbbd4dc3
Sayed-AboElela <sayed.aboelela96@gmail.com>
2025-02-07 23:07:26 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const mobileToggler = document.getElementById('btn--mobile--menu');
+//     const closeToggler = document.getElementById('btn--close');
+//     const navbarList = document.getElementById('navlist');
+
+//     function openNav() {
+//         navbarList.classList.remove('hide--navlist');
+//         navbarList.classList.add('show--navlist');
+//     }
+
+//     /* Set the width of the side navigation to 0 */
+//     function closeNav() {
+//         navbarList.classList.remove('show--navlist');
+//         navbarList.classList.add('hide--navlist');
+//     }
+
+//     mobileToggler.addEventListener('click', function () {
+//         openNav()
+//     });
+
+//     closeToggler.addEventListener('click', function () {
+//         closeNav()
+//     });
+
+
+
+// }); 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("btn--mobile--menu");
+    const closeButton = document.getElementById("btn--close");
+    const navlist = document.getElementById("navlist");
+
+    function toggleMenu() {
+        navlist.classList.toggle("show--mobile--list");
+    }
+
+    menuButton.addEventListener("click", toggleMenu);
+    closeButton.addEventListener("click", toggleMenu);
+    
+    const searchToggle = document.getElementById("search-toggle");
+    const navbar = document.getElementById("navbar");
+
+    searchToggle.addEventListener("click", function () {
+        navbar.classList.toggle("search-active");
+    });
+    
+});
