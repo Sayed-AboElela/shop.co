f261b8411f21a8b230f921af82d8527d185c06e7
Sayed-AboElela <sayed.aboelela96@gmail.com>
2025-02-10 23:22:24 +0200

    completed home-page

--- js/main.js
+++ js/main.js
@@ -1,47 +1,22 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const mobileToggler = document.getElementById('btn--mobile--menu');
-//     const closeToggler = document.getElementById('btn--close');
-//     const navbarList = document.getElementById('navlist');
-
-//     function openNav() {
-//         navbarList.classList.remove('hide--navlist');
-//         navbarList.classList.add('show--navlist');
-//     }
-
-//     /* Set the width of the side navigation to 0 */
-//     function closeNav() {
-//         navbarList.classList.remove('show--navlist');
-//         navbarList.classList.add('hide--navlist');
-//     }
-
-//     mobileToggler.addEventListener('click', function () {
-//         openNav()
-//     });
-
-//     closeToggler.addEventListener('click', function () {
-//         closeNav()
-//     });
-
-// });
 
 document.addEventListener("DOMContentLoaded", function () {
-  //   const menuButton = document.getElementById("btn--mobile--menu");
-  //   const closeButton = document.getElementById("btn--close");
-  //   const navlist = document.getElementById("navlist");
+    const menuButton = document.getElementById("btn--mobile--menu");
+    const closeButton = document.getElementById("btn--close");
+    const navlist = document.getElementById("navlist");
 
-  //   function toggleMenu() {
-  //     navlist.classList.toggle("show--mobile--list");
-  //   }
+    function toggleMenu() {
+      navlist.classList.toggle("show--mobile--list");
+    }
 
-  //   menuButton.addEventListener("click", toggleMenu);
-  //   closeButton.addEventListener("click", toggleMenu);
+    menuButton.addEventListener("click", toggleMenu);
+    closeButton.addEventListener("click", toggleMenu);
 
-  //   const searchToggle = document.getElementById("search-toggle");
-  //   const navbar = document.getElementById("navbar");
+    const searchToggle = document.getElementById("search-toggle");
+    const navbar = document.getElementById("navbar");
 
-  //   searchToggle.addEventListener("click", function () {
-  //     navbar.classList.toggle("search-active");
-  //   });
+    searchToggle.addEventListener("click", function () {
+      navbar.classList.toggle("search-active");
+    });
 
   const prev = document.getElementById("prev-btn");
   const next = document.getElementById("next-btn");
