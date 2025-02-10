037f7e80c73b6c4191e06418b072b95696035413
Sayed-AboElela <sayed.aboelela96@gmail.com>
2025-02-10 03:53:54 +0200

    block--testimonial

--- js/main.js
+++ js/main.js
@@ -22,27 +22,38 @@
 //         closeNav()
 //     });
 
-
-
-// }); 
+// });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("btn--mobile--menu");
-    const closeButton = document.getElementById("btn--close");
-    const navlist = document.getElementById("navlist");
+  //   const menuButton = document.getElementById("btn--mobile--menu");
+  //   const closeButton = document.getElementById("btn--close");
+  //   const navlist = document.getElementById("navlist");
 
-    function toggleMenu() {
-        navlist.classList.toggle("show--mobile--list");
-    }
+  //   function toggleMenu() {
+  //     navlist.classList.toggle("show--mobile--list");
+  //   }
 
-    menuButton.addEventListener("click", toggleMenu);
-    closeButton.addEventListener("click", toggleMenu);
-    
-    const searchToggle = document.getElementById("search-toggle");
-    const navbar = document.getElementById("navbar");
+  //   menuButton.addEventListener("click", toggleMenu);
+  //   closeButton.addEventListener("click", toggleMenu);
 
-    searchToggle.addEventListener("click", function () {
-        navbar.classList.toggle("search-active");
-    });
-    
+  //   const searchToggle = document.getElementById("search-toggle");
+  //   const navbar = document.getElementById("navbar");
+
+  //   searchToggle.addEventListener("click", function () {
+  //     navbar.classList.toggle("search-active");
+  //   });
+
+  const prev = document.getElementById("prev-btn");
+  const next = document.getElementById("next-btn");
+  const list = document.getElementById("testimonial--list");
+  const itemWidth = 300;
+
+  prev.addEventListener("click", () => {
+    console.log("click prev");
+
+    list.scrollLeft -= itemWidth;
+  });
+  next.addEventListener("click", () => {
+    list.scrollLeft += itemWidth;
+  });
 });
